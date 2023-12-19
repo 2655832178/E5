47395f9f3bc475fb4c30c0da4199ae92ae2e4ac8
2655832178 <124099470+2655832178@users.noreply.github.com>
2023-12-20 01:06:13 +0800

    Update 2655.py

--- 2655.py
+++ 2655.py
@@ -1,16 +1,13 @@
 import time
 
-def focus_timer(minutes):
-    seconds = minutes * 60
-    while seconds:
-        mins, secs = divmod(seconds, 60)
-        timer = '{:02d}:{:02d}'.format(mins, secs)
-        print(timer, end='\r')  # Use '\r' to overwrite the display on the same line
-        time.sleep(1)
-        seconds -= 1
-
-    print("Time's up!")
+def focus_timer_task():
+    print("Stay focused! Task executed.")
 
 if __name__ == "__main__":
-    focus_minutes = int(input("Enter the focus minutes: "))
-    focus_timer(focus_minutes)
+    try:
+        while True:
+            focus_timer_task()
+            time.sleep(1200)
+
+    except KeyboardInterrupt:
+        print("\nFocus timer interrupted. Exiting.")
